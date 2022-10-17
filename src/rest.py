140c6163785ac115e2734b1855cb7ca13a70d2a1
Vitor Nere <vitor.nere@hotmail.com>
2022-10-17 20:32:48 -0300

    Push image to ECR

--- src/rest.py
+++ src/rest.py
@@ -1,9 +1,20 @@
 from common.exceptions import CommonException
 from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 from packages.todo.controllers import TodoControler
 from packages.todo.repository import TodoRepository
 from packages.todo.services import TodoService
+
+
+def init_middlewares(app: FastAPI):
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=['*'],
+        allow_credentials=True,
+        allow_methods=['*'],
+        allow_headers=['*'],
+    )
 
 
 def init_routes(app: FastAPI):
